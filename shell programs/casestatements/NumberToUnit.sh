4b9a19aca692b71effb40f6cee152b0d4af4a753
sandipk01 <sandipkengar01@gmail.com>
2020-02-24 05:46:22 +0530

    WIP : NumberToUnit.sh

--- shell programs/casestatements/NumberToUnit.sh
+++ shell programs/casestatements/NumberToUnit.sh
@@ -8,31 +8,35 @@ read number
 #declaring variable
 i=4
 
-#check unit of a number
-while [ $next -gt 0 ]
+next=$number;
+
+while [ $i -ne 0 ]
 do
+#check unit of a number
 case $i in
 4)
-	next=$number;
    number=$(( $number%10 ))
    next=$(( $next/10 ))
    echo "$number" "unit"
    i=$(( $i-1 ))
+   echo $i
 ;;
 3)
 	number=$(( $next%10 ))
    next=$(( $next/10 ))
    echo "$number" "ten"
    i=$(( $i-1 ));
+   echo $i
 ;;
 2)
 	number=$(( $next%10 )
    next=$(( $next/10 ))
    echo "$number" "hundred"
    i=$(( $i-1 ))
+   echo $i
 ;;
 1)
- 	number=$(( $next%10 ));
+ 	number=$(( $next%10 ))
 	echo "$number" "thousand"
    i=$(( $i-1 ))
 ;;
