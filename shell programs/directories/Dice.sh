428369d363646661086fb20274d174803fcfa8d5
sandipk01 <sandipkengar01@gmail.com>
2020-02-25 07:08:32 +0530

    MODIFIED : finding minimum dice count

--- shell programs/directories/Dice.sh
+++ shell programs/directories/Dice.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
 
+#VARIABLE
 declare -A dice
+min=10
+length=0
 
+#ARRAY
 dice[1]=0
 dice[2]=0
 dice[3]=0
@@ -9,11 +13,11 @@ dice[4]=0
 dice[5]=0
 dice[6]=0
 
-
 length=${#dice[@]}
 
+#CHECK IF ANY DICE COUNT IS EQUAL TO 10 THEN STOP
 function diceRoll(){
-for (( count=1; count<length; count++ ))
+for (( count=1; count<=length; count++ ))
 do
 	if [ $(( ${dice[$count]} )) -eq 10 ]
 		then
@@ -26,7 +30,6 @@ done
 }
 
 
-
 while [ $(( "$( diceRoll )" )) -lt 10 ]
 do
 	random=$(( (RANDOM%6) + 1 ))
@@ -34,30 +37,35 @@ do
 		then
 			dice[1]=$(( ${dice[1]} + 1 ))
 	fi
-	 if [ $random -eq 2 ]
+
+	if [ $random -eq 2 ]
       then
          dice[2]=$(( ${dice[2]} + 1 ))
    fi
-	 if [ $random -eq 3 ]
+
+	if [ $random -eq 3 ]
       then
          dice[3]=$(( ${dice[3]} + 1 ))
    fi
-	 if [ $random -eq 4 ]
+
+	if [ $random -eq 4 ]
       then
          dice[4]=$(( ${dice[4]} + 1 ))
    fi
-	 if [ $random -eq 5 ]
+
+	if [ $random -eq 5 ]
       then
          dice[5]=$(( ${dice[5]} + 1 ))
    fi
-	 if [ $random -eq 6 ]
+
+	if [ $random -eq 6 ]
       then
          dice[6]=$(( ${dice[6]} + 1 ))
    fi
 done
 
-
-for (( i=1; i<length; i++ ))
+#CHECK MIN AND MAX DICE COUNT
+for (( i=1; i<=length; i++ ))
 do
 	if [ $(( ${dice[$i]} )) -eq 10 ]
 		then
@@ -65,4 +73,13 @@ do
 		else
 			echo ${i} "--" ${dice[$i]}
 	fi
+
+	if [ $(( ${dice[$i]} )) -lt $min ]
+		then
+			index=$i
+			min=$(( ${dice[$i]} ))
+	fi
 done
+
+printf "dice $index is  min $min \n"
+
