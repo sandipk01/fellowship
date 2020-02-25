7d9947d9d0e24080a3f7e88dd0a01164c4f8fa31
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-25 09:09:53 +0530

    MODIFIED : Comments are added

--- shell programs/directories/Dice.sh
+++ shell programs/directories/Dice.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -x
 
-declare -A dice
+#Write a program in the following steps
+#a. Roll a die and find the number between 1 to 6
+#b. Repeat the Die roll and find the result each time
+#c. Store the result in a dictionary
+#d. Repeat till any one of the number has reached 10 times
+#e. Find the number that reached maximum times and the one that was for minimum times
 
+#VARIABLE
+declare -A dice
+min=10
+length=0
+
+#ARRAY
 dice[1]=0
 dice[2]=0
 dice[3]=0
@@ -9,11 +20,11 @@ dice[4]=0
 dice[5]=0
 dice[6]=0
 
-
 length=${#dice[@]}
 
+#CHECK IF ANY DICE COUNT IS EQUAL TO 10 THEN STOP
 function diceRoll(){
-for (( count=1; count<length; count++ ))
+for (( count=1; count<=length; count++ ))
 do
 	if [ $(( ${dice[$count]} )) -eq 10 ]
 		then
@@ -26,7 +37,6 @@ done
 }
 
 
-
 while [ $(( "$( diceRoll )" )) -lt 10 ]
 do
 	random=$(( (RANDOM%6) + 1 ))
@@ -34,30 +44,35 @@ do
 		then
 			dice[1]=$(( ${dice[1]} + 1 ))
 	fi
-	 if [ $random -eq 2 ]
+
+	if [ $random -eq 2 ]
       then
          dice[2]=$(( ${dice[2]} + 1 ))
    fi
-	 if [ $random -eq 3 ]
+
+	if [ $random -eq 3 ]
       then
          dice[3]=$(( ${dice[3]} + 1 ))
    fi
-	 if [ $random -eq 4 ]
+
+	if [ $random -eq 4 ]
       then
          dice[4]=$(( ${dice[4]} + 1 ))
    fi
-	 if [ $random -eq 5 ]
+
+	if [ $random -eq 5 ]
       then
          dice[5]=$(( ${dice[5]} + 1 ))
    fi
-	 if [ $random -eq 6 ]
+
+	if [ $random -eq 6 ]
       then
          dice[6]=$(( ${dice[6]} + 1 ))
    fi
 done
 
-
-for (( i=1; i<length; i++ ))
+#CHECK MIN AND MAX DICE COUNT
+for (( i=1; i<=length; i++ ))
 do
 	if [ $(( ${dice[$i]} )) -eq 10 ]
 		then
@@ -65,4 +80,12 @@ do
 		else
 			echo ${i} "--" ${dice[$i]}
 	fi
+
+	if [ $(( ${dice[$i]} )) -lt $min ]
+		then
+			index=$i
+			min=$(( ${dice[$i]} ))
+	fi
 done
+
+printf "dice $index is  min $min \n"
